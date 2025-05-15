70ebcf0998e03a6e79e7c8e7639c7ca41f917d3f
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-15 17:07:31 +0530

    Inline styling

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Heading() {
   return (
     <>
-      <h1 style={{ color: "blue", textShadow: "2px 2px 3px navy" }}>
+      <h1
+        style={{
+          color: "blue",
+          textShadow: "2px 2px 3px navy",
+          border: "1px solid black",
+        }}
+      >
         My Favourite Foods
       </h1>
     </>
