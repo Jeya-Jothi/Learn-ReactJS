bc770c799b5ddda5fdddab295a74ca073ebfe7e0
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-15 16:39:05 +0530

    List images

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -3,11 +3,18 @@ import React from "react";
 function List() {
   return (
     <>
-      <ul>
-        <li>Rice</li>
-        <li>Bacon</li>
-        <li>Noodles</li>
-      </ul>
+      <img
+        src="https://media.istockphoto.com/id/153737841/photo/rice.jpg?s=612x612&w=0&k=20&c=lfO7iLT0UsDDzra0uBOsN1rvr2d5OEtrG2uwbts33_c="
+        alt="Rice"
+      />
+      <img
+        src="https://www.allrecipes.com/thmb/tnXjQLBtzLNLfi7KEIuOrVzKbWs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/158727-bacon-for-the-family-or-a-crowd-beauty-DDMFS-071-d1cd1e76787b4374b3d046d50b709c83.jpeg"
+        alt="Bacon"
+      />
+      <img
+        src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTkC876spgVHFvcOJ3QOkE0vpvs3-qDVObYX-zudwge6xkmDolLCJqs4oBIkyI6MMGD5nWEBHDDgUUf3KnIlA9YiDDD4gJOpYpCCmQewzE"
+        alt="Noodles"
+      />
     </>
   );
 }
