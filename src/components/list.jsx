3feac38bac8c5a9da8bfa84e1d62f77b82b37bb4
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-16 15:44:27 +0530

    Import everything from math.js

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import pi, { doublePi, triplePi } from "./math";
+import * as pi from "./math";
 
 function List() {
+  console.log(pi);
   return (
     <>
       <ul>
-        <li>{pi}</li>
-        <li>{doublePi()}</li>
-        <li>{triplePi()}</li>
+        <li>{pi.default}</li>
+        <li>{pi.doublePi()}</li>
+        <li>{pi.triplePi()}</li>
       </ul>
     </>
   );
