88c25f59b2f4507f903657342798ebdc7a35ba9b
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-16 12:44:27 +0530

    Custom components

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 function Heading() {
-  const customStyle = {
-    color: "orange",
-    fontSize: "30px",
-    border: "1px dashed black",
-  };
-
-  customStyle.color = "blue"; // if something happens, it will change it's color to blue.
   return (
     <>
-      <h1 style={customStyle}>My Favourite Foods</h1>
+      <h1>My Favourite Foods</h1>
     </>
   );
 }
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function List() {
-  const img = "https://picsum.photos/200"; // 200px x 200px
   return (
     <>
-      <img src={`${img}?grayscale`} alt="images" />
+      <ul>
+        <li>Rice</li>
+        <li>Bacon</li>
+        <li>Noodles</li>
+      </ul>
     </>
   );
 }
