c09b13109e3eaee338ad0b78917ab7d34689c5f6
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-16 09:57:47 +0530

    Display time and wish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
-import Heading from "./components/heading";
-import List from "./components/list";
 
 function App() {
+  let hours = new Date().getHours();
+  // hours = 13;
+  const correctHour = hours % 12 === 0 ? 12 : hours % 12;
+  const minute = new Date().getMinutes();
+  let meridiem = hours < 12 ? "AM" : "PM";
+  const time =
+    correctHour + ":" + (minute < 10 ? "0" + minute : minute) + "" + meridiem;
+  let wish = {
+    color: "",
+    textAlign: "center",
+  };
+  function content() {
+    if (hours < 12) {
+      wish.color = "red";
+      return "Good morning";
+    }
+    if (hours > 12 && hours < 18) {
+      wish.color = "green";
+      return "Good Afternoon";
+    }
+    if (hours > 18) {
+      wish.color = "blue";
+      return "Good Night";
+    }
+  }
   return (
     <div>
-      <Heading />
-      <List />
+      <h1 style={wish}>
+        {content()} - {time}
+      </h1>
     </div>
   );
 }
