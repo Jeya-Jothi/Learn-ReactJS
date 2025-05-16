9303d413d2995cacb15f5fa959a7e3a096a8437a
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-16 08:48:51 +0530

    Dynamic color change

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -6,6 +6,8 @@ function Heading() {
     fontSize: "30px",
     border: "1px dashed black",
   };
+
+  customStyle.color = "blue"; // if something happens, it will change it's color to blue.
   return (
     <>
       <h1 style={customStyle}>My Favourite Foods</h1>
