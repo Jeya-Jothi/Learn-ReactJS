ed6fecc3161c9430b0ee1339a75a62bbfe855a3f
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-19 11:40:24 +0530

    Add Avatar component below h1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import Contactcart from "./components/contactCard";
-import contacts from "./utils/contacts.js"
+import contacts from "./utils/contacts.js";
+import Avatar from "./components/avatar.jsx";
 function App() {
   console.log(contacts);
-  
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
+      <Avatar img="https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=" />
       <Contactcart
         name={contacts[0].name}
         img={contacts[0].imgURL}
-        phone={contacts[0].phone} 
+        phone={contacts[0].phone}
         mail={contacts[0].email}
       />
 
