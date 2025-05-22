5f81901db505ab77eddccc357dca191fa05f42f2
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-22 13:38:15 +0530

    Update emijipedia - arrow function

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import "./App.css";
 import Emojicard from "./components/emojiCard";
 import emojipedia from "./utils/emojipedia";
 
-function ecard(emoji) {
-  return (
-    <Emojicard
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(ecard)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emoji) => {
+          <Emojicard
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+          />;
+        })}
+      </dl>
     </div>
   );
 }
