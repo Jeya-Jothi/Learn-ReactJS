93567bd3a791ba72f7bc2a3cfe342ab97954a28a
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-22 11:49:26 +0530

    Display emojipedia meaning as list-only 100 char

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import "./App.css";
+import emojipedia from "./utils/emojipedia";
 
-let numbers = [3, 56, 2, 48, 5];
-
-function reduce() {
-  return numbers.reduce((sum, n) => sum + n);
-}
-
-function findIndex() {
-  return numbers.findIndex((n) => n === 48);
+function Meaninglist(props) {
+  return <li>{props.meaning.slice(0, 100)}</li>;
 }
 
 function App() {
   return (
     <div>
       <ul>
-        <li>
-          map() -{" "}
-          {numbers.map((n) => {
-            return n * 2 + " ";
-          })}
-        </li>
-        <li>
-          filter() -{" "}
-          {numbers.find((n) => {
-            return n > 5 + "";
-          })}
-        </li>
-        {/* <li>find() - {find()}</li> */}
-        <li>reduce() - {reduce()}</li>
-        <li>findIndex() - {findIndex()}</li>
+        {emojipedia.map((n) => {
+          return <Meaninglist meaning={n.meaning} key={n.id} />;
+        })}
       </ul>
     </div>
   );
