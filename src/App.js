1a28e722d4087a1c0393f5cff47b51835bec2e94
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-22 09:43:09 +0530

    map,filter,find,reduce,findIndex - Basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import "./App.css";
-import Emojicard from "./components/emojiCard";
-import emojipedia from "./utils/emojipedia";
 
-function ecard(emoji) {
-  return (
-    <Emojicard
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
-  );
+let numbers = [3, 56, 2, 48, 5];
+
+function map() {
+  return numbers.map((n) => n * 2) + "";
+}
+
+function filter() {
+  return numbers.filter((n) => n > 5) + "";
+}
+
+function find() {
+  return numbers.find((n) => n > 5);
+}
+
+function reduce() {
+  return numbers.reduce((sum, n) => sum + n);
+}
+
+function findIndex() {
+  return numbers.findIndex((n) => n === 48);
 }
 
 function App() {
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">{emojipedia.map(ecard)}</dl>
+      <ul>
+        <li>map() - {map()}</li>
+        <li>filter() - {filter()}</li>
+        <li>find() - {find()}</li>
+        <li>reduce() - {reduce()}</li>
+        <li>findIndex() - {findIndex()}</li>
+      </ul>
     </div>
   );
 }
