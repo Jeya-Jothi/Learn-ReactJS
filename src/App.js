3175089d71f11a461fff4ef11bf01e02c020c970
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-22 15:22:38 +0530

    Note data from notes.js, arrow function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import Heading from "./components/heading";
 import Note from "./components/note";
 import Footer from "./components/footer";
+import notes from "./utils/notes";
 
 function App() {
   return (
     <>
       <Heading />
-      <Note />
+      {notes.map((note) => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
       <Footer />
     </>
   );
