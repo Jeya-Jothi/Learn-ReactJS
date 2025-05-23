ef19ad6c1b41a646c0cebd62c217594d5a1c9a4b
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-23 16:53:43 +0530

    Dynamic data change upon currentHour-why are u still work?

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import "./App.css";
 import Login from "./components/Login";
 let isLoggedIn = false;
 
+const currentHour = new Date().getHours();
+
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      {currentHour > 12 ? <h1>Why are you still working?</h1> : null}
+    </div>
   );
 }
 
