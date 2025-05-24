9412f817ff3d585ce89b565aec572a7352fbc23e
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-24 22:42:02 +0530

    Count Decrease -1

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ function App() {
   function increase() {
     setCount(count + 1);
   }
+  function decrease() {
+    setCount(count - 1);
+  }
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
