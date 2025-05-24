6fa3a655855fd1ca78c88751744febd2e4ec8442
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-24 06:45:46 +0530

    Update to inline-arrow function

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount(count + 1);
-  }
-  function decreseCount() {
-    setCount(count - 1);
-  }
+  // function increaseCount() {
+  //   setCount(count + 1);
+  // }
+  // function decreseCount() {
+  //   setCount(count - 1);
+  // }
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={increaseCount}>⇧ Increase</button>
-      <button onClick={decreseCount}>⇩ Decrese</button>
+      <button onClick={() => setCount(count + 1)}>⇧ Increase</button>
+      <button onClick={() => setCount(count - 1)}>⇩ Decrese</button>
     </div>
   );
 }
