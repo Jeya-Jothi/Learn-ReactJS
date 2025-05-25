400c786b16156f6d45f7b9640a5576a925314aa6
Jeya Jothi <jeyajothi108@gmail.com>
2025-05-25 11:38:44 +0530

    Dynamically update time evey seconds-setInterval()

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 
-//Challenge:
-//1. Given that you can get the current time using:
-// console.log(time);
-//Show the latest time in the <h1> when the Get Time button
-//is pressed.
-
-//2. Given that you can get code to be called every second
-//using the setInterval method.
-//Can you get the time in your <h1> to update every second?
-
-//e.g. uncomment the code below to see Hey printed every second.
-// function sayHi() {
-//   console.log("Hey");
-// }
-// setInterval(sayHi, 1000);
 let time = new Date().toLocaleTimeString();
 
 function App() {
@@ -23,10 +8,14 @@ function App() {
   function getTime() {
     setTime(new Date().toLocaleTimeString());
   }
+  let intervalId;
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    getTime();
+  }, 1000);
   return (
     <div>
       <h1>{currentTime}</h1>
-      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
