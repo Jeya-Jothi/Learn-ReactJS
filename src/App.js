cb6f1a1a7601a206f58e496aed66987f3487e38f
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-18 13:13:00 +0530

    Hook(Functional) Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
-import emojipedia from "./utils/emojipedia";
-
-function Meaninglist(props) {
-  return <li>{props.meaning.slice(0, 100)}</li>;
-}
 
 function App() {
   return (
     <div>
-      <ul>
-        {emojipedia.map((n) => {
-          return <Meaninglist meaning={n.meaning} key={n.id} />;
-        })}
-      </ul>
+      <h1>Hello</h1>
     </div>
   );
 }
