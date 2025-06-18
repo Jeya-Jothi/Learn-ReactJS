88b9ba81c6a10a4d9416f7a45292a90d47611060
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-18 12:55:19 +0530

    React form-input name appear

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import "./App.css";
 
 function App() {
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
 
+  function handleOnChange(e) {
+    setName(e.target.value);
+  }
+  function handleOnclick(e) {
+    setHeadingText(name);
+    e.preventDefault(); // Doesn't go to another route; Because it's a form.
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-        onMouseOver={() => setMouseOver(true)}
-        onMouseOut={() => setMouseOver(false)}
-      >
-        Submit
-      </button>
+      <h1>Hello {headingText}</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleOnChange}
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={() => setMouseOver(true)}
+          onMouseOut={() => setMouseOver(false)}
+          onClick={handleOnclick}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
