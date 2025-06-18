3628fa6ca5839f7abb56fc2d8b3be0cdbe94a779
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-18 13:18:10 +0530

    class component syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
 }
 
 export default App;
