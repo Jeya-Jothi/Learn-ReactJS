7878511826b33f9b980a55343c86a2fa973c2b67
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-19 13:49:53 +0530

    fName, lName-Simple method-useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-import React from "react";
 
-class App extends React.Component {
-  render() {
-    return <h1>Hello</h1>;
+function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+
+  function handleClick(e) {
+    e.preventDefault();
   }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fname} {lname}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          placeholder="First Name"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+        <button onClick={handleClick}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
