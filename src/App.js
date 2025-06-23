df49ef506e6940e3eeda091dc3da7e2505e69bcd
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-23 12:02:07 +0530

    Use Spread operator

--- src/App.js
+++ src/App.js
@@ -21,25 +21,10 @@ function App() {
     const { value, name } = e.target;
 
     setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email,
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email,
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value,
-        };
-      }
+      return {
+        ...prevValue,
+        [name]: value,
+      };
     });
   }
   return (
