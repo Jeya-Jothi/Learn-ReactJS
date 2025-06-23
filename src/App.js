b971e16ef74ac2711ed09437e4329ddfb663f1d7
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-23 11:27:16 +0530

    Spread operator in Array

--- src/App.js
+++ src/App.js
@@ -1,76 +1,19 @@
-import { useState } from "react";
 import "./App.css";
 
-//CHALLENGE: Make the code in App.jsx work.
-//The final app should have a single contact
-//with fName, lName and email.
-
-//HINT: You'll need to apply the following things you learnt:
-//1. Using JS Objects with state.
-//2. Making use of previous state when changing state.
-//3. Working with forms in React.
-//4. Handing events
-
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: "",
-  });
-  function handleChange(e) {
-    const { value, name } = e.target;
+  const citrus = ["Lime", "lemon", "Orange"];
+  const fruits = ["Apple", "Banana", "Coconut"];
 
-    setContact((prevValue) => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName,
-          email: prevValue.email,
-        };
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value,
-          email: prevValue.email,
-        };
-      } else if (name === "email") {
-        return {
-          fName: prevValue.fName,
-          lName: prevValue.lName,
-          email: value,
-        };
-      }
-    });
-  }
-  return (
-    <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          name="fName"
-          placeholder="First Name"
-          onChange={handleChange}
-          value={contact.fName}
-        />
-        <input
-          name="lName"
-          placeholder="Last Name"
-          onChange={handleChange}
-          value={contact.lName}
-        />
-        <input
-          name="email"
-          placeholder="Email"
-          onChange={handleChange}
-          value={contact.email}
-        />
-        <button>Submit</button>
-      </form>
-    </div>
-  );
+  console.log(citrus);
+  console.log(fruits);
+
+  const newFruits = ["Apple", "Banana", "Coconut", ...citrus];
+  console.log(newFruits);
+
+  const inMiddle = ["Apple", ...citrus, "Banana", "Coconut"];
+  console.log(inMiddle);
+
+  return <div></div>;
 }
 
 export default App;
