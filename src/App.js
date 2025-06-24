5f67fd1b8f83d796be9956e7558762e0dbf9b711
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-24 17:58:45 +0530

    Runs when 'count' changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   useEffect(() => {
-    console.log("Runs only once when component mounts");
-  }, []); // mounts- Component appears on the screen for the first time.
+    console.log("Runs when 'count' changes");
+  }, [count]);
   return (
     <div>
-      <h1>useEffect()</h1>
-      <p>
-        useEffect is a React Hook that lets you run some code (a side effect)
-        after your component renders.
-      </p>
-      <h2>Why do we use it?</h2>
-      <p>We use useEffect for things like: </p>
-      <ul>
-        <li>Fetching data from an API </li>
-        <li>Setting up timers or intervals </li>
-        <li>Updating the DOM </li>
-        <li>Listening for events (like keyboard or mouse)</li>
-      </ul>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>+</button>
     </div>
   );
 }
