5d6d956e75944ee09bde22f3378f13ff4eab43b7
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-24 15:05:55 +0530

    Ending code- To-do list(useState, Spread operator)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  //CHALLENGE: Make this app work by applying what you've learnt.
-  //1. When new text is written into the input, its state should be saved.
-  //2. When the add button is pressed, the current data in the input should be
-  //added to an array.
-  //3. The <ul> should display all the array items as <li>s
+  const [newtext, setNewText] = useState("");
+  const [content, setContent] = useState([]);
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setNewText(newValue);
+  }
+  function handleClick() {
+    setContent((prevVlaue) => {
+      return [newtext, ...prevVlaue];
+    });
+    setNewText("");
+    // console.log(content); // It shows first, so we can use useEffect for correct content.
+  }
+
+  useEffect(() => {
+    console.log("Updated content:", content);
+  }, [content]);
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={newtext} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {content.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
