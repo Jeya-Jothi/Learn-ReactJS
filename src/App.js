52f328b5d25df52423c546cfa85d34db8c708b26
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-24 17:47:11 +0530

    Runs eveytime when component updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
-import emojipedia from "./utils/emojipedia";
-
-function Meaninglist(props) {
-  return <li>{props.meaning.slice(0, 100)}</li>;
-}
 
 function App() {
+  useEffect(() => {
+    console.log("Hello from useEffect");
+  }); // It runs every time the component updates (re-renders).
   return (
     <div>
+      <h1>useEffect()</h1>
+      <p>
+        useEffect is a React Hook that lets you run some code (a side effect)
+        after your component renders.
+      </p>
+      <h2>Why do we use it?</h2>
+      <p>We use useEffect for things like: </p>
       <ul>
-        {emojipedia.map((n) => {
-          return <Meaninglist meaning={n.meaning} key={n.id} />;
-        })}
+        <li>Fetching data from an API </li>
+        <li>Setting up timers or intervals </li>
+        <li>Updating the DOM </li>
+        <li>Listening for events (like keyboard or mouse)</li>
       </ul>
     </div>
   );
